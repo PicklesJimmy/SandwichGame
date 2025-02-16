cb7c852c65e0ca8311d471c9be7942f6c12234e6
Jimmy <zqi10@students.cs.ubc.ca>
2025-02-15 23:50:38 -0800

    Added specification for the SandwichGame class from the ui package

---
+++ src/main/ui/SandwichGame.java
@@ -0,0 +1,71 @@
+package ui;
+
+import model.Bacon;
+import model.Ingredients;
+import model.Pickle;
+import model.Sandwich;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+// A sandwich game that allows the player to make and serve sandwiches to customers
+public class SandwichGame {
+
+   
+    // EFFECTS: Creates an instance of the sandwich game in the terminal console
+    public SandwichGame() {
+       // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initializes the game with the starting values
+    public void inital() {
+        // stub
+    }
+
+     // EFFECTS: displays and checks the user's inputs for the main game menu.
+     public void mainGameMenu() {
+       // stub
+
+     }
+
+    // EFFECTS: processes the user's input in the game 
+    public void processInput(String input) {
+       // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds a new bacon to the sandwich
+    public void addBacon() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds a new pickle to the sandwich
+    public void addPickle() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: displays all the ingredients inside the sandwich
+    public void viewSandwich() {
+       // stub
+    }
+     
+    // MODIFIES: this
+    // EFFECTS: the last ingredient added from the sandwich
+    public void removeIngredient() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Prints a message to tell the user that the game has stopped and stops the game from running
+    public void exitGame() {
+        // stub
+    }
+
+
+}
