d0df5faab4189039275d18aab55e86861fa52f14
Jimmy <zqi10@students.cs.ubc.ca>
2025-03-02 00:42:10 -0800

    Added class level documentation for the Writable class

--- src/main/persistence/Writable.java
+++ src/main/persistence/Writable.java
@@ -2,6 +2,7 @@ package persistence;
 
 import org.json.JSONObject;
 
+// Represents an interface for objects that can be converted to a JSON file
 public interface Writable {
     // EFFECTS: returns this as JSON object
     JSONObject toJson();
