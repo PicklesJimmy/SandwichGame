36e4692da8f92f7eb6563db0c5c3716661f0431d
Jimmy <zqi10@students.cs.ubc.ca>
2025-03-22 16:16:07 -0700

    changed the main class so SandwichGameGui is ran instead of the console game

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -3,7 +3,7 @@ package ui;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to my sandwich simulator game project!");
-        new SandwichGame();
+        new SandwichGameGui();
     }
 
 }
